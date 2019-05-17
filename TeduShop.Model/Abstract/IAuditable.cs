8f9711f1fa895512e6b6382c6826d1105c081336
Nhuongkieu <uchihadet@gmail.com>
2019-05-17 12:34:01 +0700

    The first commit

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+
+        DateTime? CreatedDate { get; set; }
+        string Createdby { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        string UpdatedBy { get; set; }
+
+        bool Status { get; set; }
+
+    }
+}
---
+++ TeduShop.Model/Models/VisistorStatistic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("VisistorStatistics")]
+    public class VisistorStatistic
+    {
+        [Key]
+        public Guid ID { get; set; }
+        
+        [Required]
+        public DateTime VistedDate { get; set; }
+
+        [MaxLength(50)]
+        public string IPAddress { get; set; }
+    }
+}
